00aa846aef180517a3b27c3fa20f3d93e5726f85
Carlos Jimenez <carlos.exejimenez@gmail.com>
2024-08-08 00:51:43 -0300

    Update navigator styles

--- App.tsx
+++ App.tsx
@@ -1,27 +1,21 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import CameraScreen from './screen/CameraScreen';
+import QRScanner from './screen/QRScanner';
 
 const Stack = createStackNavigator();
 
-export default function App() {
+const App = () => {
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
-      <Stack.Navigator>
-        <Stack.Screen name="Camera" component={CameraScreen} />
+      <Stack.Navigator initialRouteName='Login' screenOptions={{headerShown: false}}>
+        <Stack.Screen name='Login' component={QRScanner} />
+        <Stack.Screen name='Camera' component={CameraScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
