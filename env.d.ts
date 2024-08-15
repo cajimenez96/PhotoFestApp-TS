64fa4321791a9cac4ab9aa74be507f83178cdc06
agustintarancon <agustinbuf@gmail.com>
2024-08-15 20:36:06 -0300

    Configuracion de variables de entorno

---
+++ env.d.ts
@@ -0,0 +1,10 @@
+declare module '@env' {
+  export const VITE_API_KEY = string;
+  export const VITE_AUTH_DOMAIN = string;
+  export const VITE_PROJECT_ID = string;
+  export const VITE_STORAGE_BUCKET = string;
+  export const VITE_MESSAGING_SENDER_ID = string;
+  export const VITE_APP_ID = string;
+  export const VITE_MEASUREMENT_ID = string;
+  export const VITE_API_URL = string;
+}
--- firebase/firebase.config.ts
+++ firebase/firebase.config.ts
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
+import { VITE_API_KEY, VITE_AUTH_DOMAIN, VITE_PROJECT_ID, VITE_STORAGE_BUCKET, VITE_MESSAGING_SENDER_ID, VITE_APP_ID, VITE_MEASUREMENT_ID, } from "@env"
 
 const firebaseConfig = {
-  apiKey: process.env.VITE_API_KEY,
-  authDomain: process.env.VITE_AUTH_DOMAIN,
-  projectId: process.env.VITE_PROJECT_ID,
-  storageBucket: process.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
-  appId: process.env.VITE_APP_ID,
-  measurementId: process.env.VITE_MEASUREMENT_ID,
+  apiKey: VITE_API_KEY,
+  authDomain: VITE_AUTH_DOMAIN,
+  projectId: VITE_PROJECT_ID,
+  storageBucket: VITE_STORAGE_BUCKET,
+  messagingSenderId: VITE_MESSAGING_SENDER_ID,
+  appId: VITE_APP_ID,
+  measurementId: VITE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
