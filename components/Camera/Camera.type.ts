b9f9f5839ccdc9b4b7d2dca342566e30bb70c47d
Carlos Jimenez <carlos.exejimenez@gmail.com>
2024-08-15 16:15:24 -0300

    Update components

--- components/Camera/Camera.type.ts
+++ components/Camera/Camera.type.ts
@@ -1,8 +1,12 @@
-import { BarcodeScanningResult } from "expo-camera";
+import { BarcodeScanningResult, CameraMode, CameraType, CameraView, FlashMode } from "expo-camera";
 import { ReactNode } from "react";
 
 export interface ICamera {
   children: ReactNode;
   torch?: boolean;
+  mode?: CameraMode;
+  facing?: CameraType;
+  flash?: FlashMode;
+  ref?: React.LegacyRef<CameraView>;
   handleBarCodeScanned?: (scanningResult: BarcodeScanningResult) => void;
 }
