9d8728e47d73e68dd1e931f2f7f5c203bc833f90
agustintarancon <agustinbuf@gmail.com>
2024-09-04 11:11:44 -0300

    userID

--- screen/CameraScreen/require.ts
+++ screen/CameraScreen/require.ts
@@ -1,11 +1,15 @@
 import { MediafileRequest } from "../../api/mediafile"
 import { Alert } from "react-native"
+import { getAsyncStorage } from "../../helpers/helper";
+import { USER_ID } from "../../common/constants";
 
 export async function sendToBackend(downloadURL: string, width: number, height: number, mediaTypeId: string) {
+  const userId = await getAsyncStorage(USER_ID) ?? '';
+
   const payload = {
     EventID: "66b6ab67d728b05b6b08ab42",
     MediaURL: downloadURL,
-    UserID: "66b548239d87a872c6b141ca",
+    UserID: userId,
     Width: `${width}px`,
     height: `${height}px`,
     MediaTypeID: mediaTypeId,
