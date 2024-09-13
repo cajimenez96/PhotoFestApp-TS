7085ab7189bbffc34897912e0fe6de71d118bdb5
agustintarancon <agustinbuf@gmail.com>
2024-09-13 15:57:53 -0300

    Nuevo manejo de screens

--- App.tsx
+++ App.tsx
@@ -1,34 +1,19 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { StatusBar } from 'expo-status-bar';
 import CameraScreen from './screen/CameraScreen';
 import QRScanner from './screen/QRScanner';
-import { getAsyncStorage } from './helpers/helper';
-import { useEffect, useState } from 'react';
-import { TOKEN } from './common/constants';
-
-const Stack = createStackNavigator();
+import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
 
 const App = () => {
-  const [newToken, setNewToken] = useState<string | null>();
-
-  useEffect(() => {
-    const updateToken = async () => {
-      const token = await getAsyncStorage(TOKEN);
-      setNewToken(token);
-    }
-
-    updateToken();
-  }, []);
+  const [userLogued, setUserLogued] = useState<boolean>(false)
 
   return (
-    <NavigationContainer>
+    <>
       <StatusBar hidden />
-      <Stack.Navigator initialRouteName={newToken ? 'Camera' : 'Login'} screenOptions={{headerShown: false}}>
-        <Stack.Screen name='Login' component={QRScanner} />
-        <Stack.Screen name='Camera' component={CameraScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      {!userLogued ?
+        <QRScanner setUserLogued={setUserLogued }/> :
+        <CameraScreen /> 
+      }                    
+    </>
   );
 }
 
--- screen/QRScanner/QRScanner.data.ts
+++ screen/QRScanner/QRScanner.data.ts
@@ -1,5 +1,6 @@
 export const QRScannerData = {
   text: `Escaneá el QR para \niniciar sesión`,
   loginError: "Las credenciales no son correctas. Por favor, verifica el código QR y escanéalo nuevamente.",
-  qrError: "QR invalido"
+  qrError: "QR invalido",
+  userError: "Usuario y/o contraseña incorrecto, intente nuevamente."
 }
