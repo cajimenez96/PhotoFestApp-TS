a78b65897c187e55c4a3fa8c2115bd2444ddb2fc
Carlos <carlos.exejimenez@gmail.com>
2024-09-27 11:15:47 -0300

    Se agrega componente Input

---
+++ components/Input/index.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, TextInput } from 'react-native';
+import { IInput } from './Input.types';
+
+const Input = ({placeholder = "", style}: IInput) => {
+  const [text, onChangeText] = useState(placeholder);
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        autoComplete="email"
+        style={[styles.input, style]}
+        onChangeText={onChangeText}
+        placeholder={placeholder}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 52,
+    borderWidth: 1.5,
+    padding: 10,
+    borderRadius: 12,
+    borderColor: "#767676",
+    textAlign: 'center'
+  },
+});
+
+export default Input;
