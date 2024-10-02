c01decae0c26b493601ffdb7d2b69bad7a4eb3e7
agustintarancon <agustinbuf@gmail.com>
2024-10-02 17:21:07 -0300

    Se implementa el modal de vista previa para fotos y videos

--- helpers/cameraActions.ts
+++ helpers/cameraActions.ts
@@ -8,29 +8,34 @@ import { MediaTypePicture, MediaTypeVideo, VIDEO } from '../common/constants';
 import { Alert } from 'react-native';
 import * as ImagePicker from "expo-image-picker"
 
-const saveToLibrary = async (filename: string, mediaLibraryPermission: any, requestMediaLibraryPermission: () => Promise<any>) => {
-  if (!mediaLibraryPermission.granted) {
-    const { status } = await requestMediaLibraryPermission();
-    if (status !== 'granted') {
-      console.error('Permission to access media library is required!');
-      return;
-    }
-  }
+const saveToLibrary = async (filename: string) => {
   const asset = await MediaLibrary.createAssetAsync(filename);
   await MediaLibrary.createAlbumAsync('FestBook', asset, false);
   return asset;
 };
 
+export const uploadMedia = async (mediaUri: string, type: "picture" | 'video') => {
+  const isPhoto = type === "picture";
+  const name = isPhoto ? `photo_${Date.now()}.jpg` : `video_${Date.now()}.mp4`;
+  const filename = FileSystem.documentDirectory + name;
+
+  await FileSystem.copyAsync({ from: mediaUri, to: filename });
+  const asset = await saveToLibrary(filename);
+
+  if (asset) {
+    const downloadURL = await uploadFile(mediaUri, name);
+    if (downloadURL) {
+      await sendToBackend(downloadURL, asset.width, asset.height, isPhoto ? MediaTypePicture : MediaTypeVideo);
+    }
+  }
+};
+
 export const takePicture = async (
   setLoading: Dispatch<SetStateAction<boolean>>,
   cameraRef: React.MutableRefObject<CameraView | null>,
-  mediaLibraryPermission: MediaLibrary.PermissionResponse,
-  requestMediaLibraryPermission: () => Promise<MediaLibrary.PermissionResponse>
+  setPicture: React.Dispatch<React.SetStateAction<string>>,
 ) => {
   setLoading(true);
-  setTimeout(() => {
-    setLoading(false);
-  }, 1500);
 
   if (cameraRef.current) {
     try {
@@ -44,21 +49,12 @@ export const takePicture = async (
       const picture = await cameraRef.current.takePictureAsync(options);
 
       if (picture) {
-        const namePhoto = `photo_${Date.now()}.jpg`;
-        const filename = FileSystem.documentDirectory + namePhoto;
-
-        await FileSystem.copyAsync({ from: picture.uri, to: filename });
-        const asset = await saveToLibrary(filename, mediaLibraryPermission, requestMediaLibraryPermission);
-
-        if (asset) {
-          const downloadURL = await uploadFile(picture.uri, namePhoto);
-          if (downloadURL) {
-            await sendToBackend(downloadURL, asset.width, asset.height, MediaTypePicture);
-          }
-        }
+        setPicture(picture.uri)
       }
     } catch (error) {
-      Alert.alert("Error al guardar", "Ha ocurrido un error al guardar la foto")
+      console.log(error)
+    } finally {
+      setLoading(false)
     }
   }
 };
@@ -67,9 +63,8 @@ export const takeVideo = async (
   cameraRef: React.MutableRefObject<CameraView | null>,
   isRecording: boolean,
   setIsRecording: Dispatch<SetStateAction<boolean>>,
-  mediaLibraryPermission: MediaLibrary.PermissionResponse,
-  requestMediaLibraryPermission: () => Promise<MediaLibrary.PermissionResponse>,
   setLoading: Dispatch<SetStateAction<boolean>>,
+  setVideo: React.Dispatch<React.SetStateAction<string>>
 ) => {
   setLoading(true);
   setTimeout(() => {
@@ -85,17 +80,7 @@ export const takeVideo = async (
         setIsRecording(true);
         const video = await cameraRef.current.recordAsync();
         if (video) {
-          const videoName = `video_${Date.now()}.mp4`;
-          const filename = FileSystem.documentDirectory + videoName;
-          await FileSystem.copyAsync({ from: video.uri, to: filename });
-          const asset = await saveToLibrary(filename, mediaLibraryPermission, requestMediaLibraryPermission);
-
-          if (asset) {
-            const downloadURL = await uploadFile(video.uri, videoName);
-            if (downloadURL) {
-              await sendToBackend(downloadURL, asset?.width, asset?.height, MediaTypeVideo);
-            }
-          }
+          setVideo(video.uri)
         }
       }
     } catch (error) {
@@ -123,7 +108,7 @@ export const pickImage = async (
     for (const asset of result.assets) {
       if (totalFiles > 1) setUploadStatus(`Subiendo archivos`);
       else setUploadStatus(`Subiendo archivo`)
- 
+
       if (asset.uri && asset.fileName) {
         const downloadURL = await uploadFile(asset.uri, asset.fileName);
         if (downloadURL) {
--- screen/CameraScreen/index.tsx
+++ screen/CameraScreen/index.tsx
@@ -11,6 +11,7 @@ import { pickImage, takePicture, takeVideo } from '../../helpers/cameraActions';
 import * as MediaLibrary from 'expo-media-library';
 import Slider from '@react-native-community/slider';
 import { CameraActionButtonProps } from './CameraScreen.type';
+import ModalPreview from '../../components/ModalPreview/ModalPreview';
 
 const CameraActionButton = ({ onPress, img }: CameraActionButtonProps) => {
   return (
@@ -24,9 +25,12 @@ const CameraScreen = () => {
   const { facing, toggleFlash, flash, toggleCameraFacing, toggleCameraModePhoto, toggleCameraModeVideo, mode, isRecording, setIsRecording, zoom, setZoom } = useCamera();
   const cameraRef = useRef<CameraView>(null);
   const [loading, setLoading] = useState<boolean>(false);
-  const [mediaLibraryPermission, requestMediaLibraryPermission] = MediaLibrary.usePermissions();
+  const [mediaLibraryPermission] = MediaLibrary.usePermissions();
   const [successUpload, setSuccessUpload] = useState<boolean>(false);
   const [uploadStatus, setUploadStatus] = useState<string>('');
+
+  const [picture, setPicture] = useState<string>('');
+  const [video, setVideo] = useState<string>('');
 
   if (!mediaLibraryPermission) {
     return <View />;
@@ -34,9 +38,9 @@ const CameraScreen = () => {
 
   const pictureOrVideo = () => {
     if (mode === "picture") {
-      loading ? undefined : takePicture(setLoading, cameraRef, mediaLibraryPermission, requestMediaLibraryPermission)
+      loading ? undefined : takePicture(setLoading, cameraRef, setPicture)
     } else {
-      loading ? undefined : takeVideo(cameraRef, isRecording, setIsRecording, mediaLibraryPermission, requestMediaLibraryPermission, setLoading)
+      loading ? undefined : takeVideo(cameraRef, isRecording, setIsRecording, setLoading, setVideo)
     }
   }
 
@@ -46,6 +50,15 @@ const CameraScreen = () => {
       setUploadStatus, 
     );
   };
+
+  if (picture) {
+    return <ModalPreview media={picture} setMedia={setPicture} mediaType='picture'/>
+  }
+
+  if (video) {
+    return <ModalPreview media={video} setMedia={setVideo} mediaType='video'/>
+  }
+
 
   return (
     <View style={globalStyles.container}>
@@ -87,11 +100,11 @@ const CameraScreen = () => {
           </View>
           <View style={styles.buttonBot}>
             <CameraActionButton
-              onPress={toggleCameraModeVideo}
+              onPress={isRecording ? () => {}  : toggleCameraModeVideo}
               img={mode === VIDEO ? cameraIcons.videoModeDark : cameraIcons.videoMode}
             />
             <CameraActionButton
-              onPress={toggleCameraModePhoto}
+              onPress={isRecording ? () => {}  : toggleCameraModePhoto}
               img={mode === PICTURE ? cameraIcons.pictureModeDark : cameraIcons.pictureMode}
             />
           </View>
