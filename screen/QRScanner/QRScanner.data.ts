d578e49374f8024ca16a3b27dd6581a72a76df5c
Matias Roger Narcotti <matiasrogern.dev@gmail.com>
2024-11-08 12:59:37 -0300

    refactor: se emplean constantes para mensajes de feedback

--- screen/QRScanner/QRScanner.data.ts
+++ screen/QRScanner/QRScanner.data.ts
@@ -2,5 +2,14 @@ export const QRScannerData = {
   text: `Escaneá el QR para \niniciar sesión`,
   loginError: "Las credenciales no son correctas. Por favor, verifica el código QR y escanéalo nuevamente.",
   qrError: "QR invalido",
-  userError: "Usuario y/o contraseña incorrecto, intente nuevamente."
+  userError: "Usuario y/o contraseña incorrecto, intente nuevamente.",
+  emailError: "Por favor, ingrese un correo electrónico válido.",
+  nameError: "El nombre debe tener un máximo de 50 caracteres",
+  lastNameError: "El apellido debe tener un máximo de 50 caracteres",
+}
+
+export const INPUT = {
+  NAME: 'Nombre',
+  LASTNAME: 'Apellido',
+  EMAIL: 'Correo electrónico',
 }
