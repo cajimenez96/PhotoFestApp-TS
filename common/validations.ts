feeabcd9c0d531ff98aaeb204609e98ffa44e2a6
Matias Roger Narcotti <matiasrogern.dev@gmail.com>
2024-11-08 12:57:34 -0300

    feat: se agrega validacion de nombre y apellido de usuario

--- common/validations.ts
+++ common/validations.ts
@@ -2,3 +2,8 @@ export const isValidEmail = (email: string): boolean => {
   const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return emailPattern.test(email);
 };
+
+export const isValidName = (name: string): boolean => {
+  const nameRegex =/^[A-Za-zÁÉÍÓÚáéíóúÜüñÑ' ]{2,50}$/;
+  return nameRegex.test(name);
+};
